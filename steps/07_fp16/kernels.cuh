ab2f32a23a13b931832f1eca923a47bec5e35e2a
iuliet716 <iuliet716@gmail.com>
2026-07-25 06:59:24 +0900

    Add step 07

--- steps/07_fp16/kernels.cuh
+++ steps/07_fp16/kernels.cuh
@@ -3,8 +3,7 @@
 #include <cuda_fp16.h>
 #include <cuda_runtime.h>
 
-// maximum head dimension (must be a multiple of 64: 16-byte chunks are
-// 8 halves, and the XOR swizzle permutes chunks in groups of 8)
+// maximum head dimension (multiple of 64 for 8-chunk XOR swizzle)
 constexpr int FUSED_D_MAX = 128;
 
 void launch_fused_attention(
